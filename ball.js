abb08b98550e800b95e3c3bf2344d79006dbb439
aaravtayal <72193828+aaravtayal@users.noreply.github.com>
2020-11-08 07:53:19 -0800

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,29 @@
+class ball{
+    constructor(x, y,radius) {
+        var options = {
+             isStatic:false,
+            restitution:0.9,
+            friction:1.0,
+            density:1.0
+        }
+        this.radius = radius;
+        this.body = Matter.Bodies.circle(x, y,radius, options);
+        World.add(world, this.body);
+      }
+      display()
+      {
+
+      //  Engine.update(engine);
+        push();
+         ellipseMode(RADIUS)
+         fill("magenta")
+         ellipse(this.body.position.x,this.body.position.y,this.radius,this.radius);
+        pop();
+        }
+}
+
+
+
+
+
+
